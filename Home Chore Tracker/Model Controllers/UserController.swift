eac16ef4ba4bd2148e6737f3fcef4e57cccba3ef
ladybeitel <ciarabeitel@gmail.com>
2019-10-21 18:50:53 -0400

    Add in Jerry's storyboards and view controller files.

---
+++ Home Chore Tracker/Model Controllers/UserController.swift
@@ -0,0 +1,9 @@
+//
+//  UserController.swift
+//  Home Chore Tracker
+//
+//  Created by Ciara Beitel on 10/21/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
---
+++ Home Chore Tracker/Views/ChoreTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ChoreTableViewCell.swift
+//  Home Chore Tracker
+//
+//  Created by Ciara Beitel on 10/21/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ChoreTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
